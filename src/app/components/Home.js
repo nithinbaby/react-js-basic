0b28a68e746325d86251c5d59383a802ef6ffc4a
Nithin Baby <nitbaby@gmail.com>
2019-10-13 22:49:06 +0530

    multiple components wired together

---
+++ src/app/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Im in a new component!</p>
+        {2+5}
+      </div>
+
+    );
+  }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
+import {Header} from "./components/Header";
+import {Home} from "./components/Home";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Hello !</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Header/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <Home/>
+          </div>
+        </div>
       </div>
     );
   }
