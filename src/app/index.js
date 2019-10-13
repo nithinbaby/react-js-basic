fe406fa39507f6a44d8ae18047a14f68d99de405
Nithin Baby <nitbaby@gmail.com>
2019-10-13 19:30:58 +0530

    first react component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,14 @@
-console.log("Intro to React JS");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello !</h2>
+      </div>
+    );
+  }
+}
+
+render(<App/>, window.document.getElementById("app"));
